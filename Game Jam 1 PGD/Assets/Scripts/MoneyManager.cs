2a014fbe513e5893495a5376baf0bfb6e0e6fe0b
Ramon <ramon.bleeker2@hva.nl>
2021-09-14 16:42:17 +0200

    Timer & Money
    +Added a timer that fills up over time
    +Added UI to show money

---
+++ Game Jam 1 PGD/Assets/Scripts/MoneyManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int money = 0;
+
+    [SerializeField] private Text moneyText;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M)) 
+        {
+            money += 50;
+        }
+
+
+        moneyText.text = "Money: " + money;
+    }
+}
---
+++ Game Jam 1 PGD/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    [SerializeField] private Slider timeSlider;
+    [SerializeField] private Text timeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeSlider.value = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSlider.value += Time.deltaTime;
+
+        if (timeSlider.value >= timeSlider.maxValue) 
+        {
+            timeText.text = "Finished";
+        }
+    }
+}
